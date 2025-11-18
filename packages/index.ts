39e88197707c5655788890ae4c1745f2d785ff8f
fract <87593133+inoUwU@users.noreply.github.com>
2025-11-19 00:25:01 +0900

    createApp実装

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello ,world");
+export type Option = {
+  render: () => string;
+};
+
+export type App = {
+  mount: (selector: string) => void;
+};
+
+export const createApp = (option: Option): App => {
+  return {
+    mount: (selector) => {
+      const root = document.querySelector(selector);
+      if (root) {
+        root.innerHTML = option.render();
+      }
+    },
+  };
+};
